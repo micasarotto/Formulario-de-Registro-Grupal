1d3bd2d7de88c0b33ef53f0878ca99cbb30e7679
Micaela Sarotto <micaelasarotto@gmail.com>
2024-08-15 15:03:01 -0300

    cambio

--- js/validacion.js
+++ js/validacion.js
@@ -7,7 +7,7 @@ function showAlertError() {
 }
 
 document.getElementById('regBtn').addEventListener('click', function () {
-   
+
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const email = document.getElementById('email').value;
