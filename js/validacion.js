13487e82c9aa610c93011175641475040f2b62b3
PilarRodriguez27 <rodriguezpilar037@gmail.com>
2024-08-15 15:41:17 -0300

    cambio

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,5 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+ //
+ if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "") {
+    showAlertError();
+    return;
 }
