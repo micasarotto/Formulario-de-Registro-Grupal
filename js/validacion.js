dfe00326ed1500c499f240d65657e7d2a698ace1
Paula García <p.garcialopez@outlook.com>
2024-08-15 15:03:31 -0300

    p.5

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,8 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+if (password1 !== password2) {
+    showAlertError();
+    return;
+}
