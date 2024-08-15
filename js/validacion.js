242c91ae01f124dc1d3a33f178df80d79e36fc20
Miranda Stankevicius <mirandastankev@gmail.com>
2024-08-15 14:48:22 -0300

    comentarios

--- js/validacion.js
+++ js/validacion.js
@@ -5,30 +5,41 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+    
+    //
 document.getElementById('regBtn').addEventListener('click', function () {
 
+    
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const email = document.getElementById('email').value;
+
+   //
     const password1 = document.getElementById('password1').value;
     const password2 = document.getElementById('password2').value;
     const terminos = document.getElementById('terminos').checked;
 
+
+    //
     if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "") {
         showAlertError();
         return;
     }
 
+    //
     if (password1.length < 6) {
         showAlertError();
         return;
     }
 
+    //
     if (password1 !== password2) {
         showAlertError();
         return;
     }
 
+    // 
     if (!terminos) {
         showAlertError();
         return;
