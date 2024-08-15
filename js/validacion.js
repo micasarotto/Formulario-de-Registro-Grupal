29afa9c919d239ea0dbf5aded16624b1e571b7e8
Micaela Sarotto <micaelasarotto@gmail.com>
2024-08-15 15:01:40 -0300

    getElementById

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,11 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById('regBtn').addEventListener('click', function () {
+   
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+
+}
