a438b82efe61a32d24b2342ea4587bc6d8eb06e3
elianaalonso <elianaalonsocastro@gmail.com>
2024-08-15 15:15:04 -0300

    version final

--- js/validacion.js
+++ js/validacion.js
@@ -1,12 +1,4 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
-
-//4
 if (password1.length < 6) {
     showAlertError();
     return;
