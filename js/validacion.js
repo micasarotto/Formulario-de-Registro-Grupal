dc1b6b6db7f3eb7784873cf7ca0f7d33bef568e7
camilacuitino23 <camilacuitino23@gmail.com>
2024-08-15 15:35:39 -0300

    add if

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,8 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+if (!terminos) {
+    showAlertError();
+    return;
+}
