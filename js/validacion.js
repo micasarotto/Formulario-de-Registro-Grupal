6f3948976aa9eef3535f0c42b4ff5c56dd404af6
PilarRodriguez27 <rodriguezpilar037@gmail.com>
2024-08-15 15:15:04 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,10 +1,4 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
  //
  if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "") {
     showAlertError();
