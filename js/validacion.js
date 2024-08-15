439a6a21fbd2de2855099c264d0e8bd3ac1e8442
Miranda Stankevicius <mirandastankev@gmail.com>
2024-08-15 15:04:14 -0300

    ..

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,8 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+const password1 = document.getElementById('password1').value;
+const password2 = document.getElementById('password2').value;
+const terminos = document.getElementById('terminos').checked;
