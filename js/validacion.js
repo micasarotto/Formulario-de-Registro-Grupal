190368a1493732ec71c64736d2e5a831e28314aa
Miranda Stankevicius <mirandastankev@gmail.com>
2024-08-15 14:57:23 -0300

    numeros

--- js/validacion.js
+++ js/validacion.js
@@ -7,7 +7,7 @@ function showAlertError() {
 }
 
     
-    //
+    // 1
 document.getElementById('regBtn').addEventListener('click', function () {
 
     
@@ -15,31 +15,31 @@ document.getElementById('regBtn').addEventListener('click', function () {
     const apellido = document.getElementById('apellido').value;
     const email = document.getElementById('email').value;
 
-   //
+   //2
     const password1 = document.getElementById('password1').value;
     const password2 = document.getElementById('password2').value;
     const terminos = document.getElementById('terminos').checked;
 
 
-    //
+    //3
     if (nombre === "" || apellido === "" || email === "" || password1 === "" || password2 === "") {
         showAlertError();
         return;
     }
 
-    //
+    //4
     if (password1.length < 6) {
         showAlertError();
         return;
     }
 
-    //
+    //5
     if (password1 !== password2) {
         showAlertError();
         return;
     }
 
-    // 
+    // 6
     if (!terminos) {
         showAlertError();
         return;
